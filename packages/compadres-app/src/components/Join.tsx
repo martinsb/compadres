06b6cbca7514ab0c1b5ca1e9205eee8e34c19ed9
Martins Barinskis <martins@barinskis.me>
2022-05-19 20:15:19 +0300

    some join screen customization

--- packages/compadres-app/src/components/Join.tsx
+++ packages/compadres-app/src/components/Join.tsx
@@ -1,6 +1,11 @@
 import React, { useRef } from "react";
 import styled from "styled-components";
 
+const JoinElement = styled.div`
+  margin: var(--spacing) auto;
+  width: 400px;
+`;
+
 const Error = styled.p`
   color: var(--form-element-invalid-border-color);
 `;
@@ -14,7 +19,7 @@ interface JoinProps {
 export const Join: React.FC<JoinProps> = ({ onLogin, errorMessage, defaultName }) => {
   const nameInputRef = useRef<HTMLInputElement>(null);
   return (
-    <div>
+    <JoinElement>
       <p>Please enter any name</p>
       {errorMessage && <Error>{errorMessage}</Error>}
       <input ref={nameInputRef} type="text" defaultValue={defaultName} />
@@ -25,6 +30,6 @@ export const Join: React.FC<JoinProps> = ({ onLogin, errorMessage, defaultName }
       >
         Join
       </button>
-    </div>
+    </JoinElement>
   );
 };
