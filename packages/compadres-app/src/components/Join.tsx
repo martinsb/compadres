588c766418cbe01f3923a1bf24af969efb2591e5
Martins Barinskis <martins@barinskis.me>
2022-05-19 21:12:26 +0300

    add title and logo

--- packages/compadres-app/src/components/Join.tsx
+++ packages/compadres-app/src/components/Join.tsx
@@ -1,6 +1,31 @@
 import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 
+const Header = styled.header`
+  position: relative;
+  height: 100px;
+  padding-left: 130px;
+  margin-bottom: calc(var(--spacing) * 3);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  user-select: none;
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 110px;
+    height: 110px;
+    border-radius: 50%;
+    background: white url('/logo.png') no-repeat center;
+  }
+
+  h1, h6 {
+    margin: 0;
+  }
+`;
+
 const JoinElement = styled.div`
   margin: var(--spacing) auto;
   width: 400px;
@@ -23,6 +48,10 @@ export const Join: React.FC<JoinProps> = ({ onLogin, errorMessage, defaultName }
   }, []);
   return (
     <JoinElement>
+      <Header>
+        <h1>Compadres</h1>
+        <h6>Completing things together</h6>
+      </Header>
       <p>Please enter any name</p>
       {errorMessage && <Error>{errorMessage}</Error>}
       <input ref={nameInputRef} type="text" defaultValue={defaultName} />
